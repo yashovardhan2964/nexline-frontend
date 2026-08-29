7b209da11dd8390740e58ed727c31df4a44966b4
Yashovardhan Singh <singh.yashovardhan04@gmail.com>
2026-08-30 02:37:41 +0530

    Use environment variable for API URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for Spring Boot backend
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
 // Axios instance with default config
 const api = axios.create({
